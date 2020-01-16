0fd9d2686c599811b4eed040d4320d4ba33485f6
pxgraham <pxgraham@gmail.com>
2020-01-16 09:35:32 -0700

    fix deploy

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Party Woo</Text>
+      <Text>Party Woooooooo</Text>
     </View>
   );
 }
