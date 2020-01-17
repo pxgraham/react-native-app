b982f5c60e3b58b8b4454230a62420d2b6b22533
unknown <pxgraham@gmail.com>
2020-01-16 21:05:51 -0700

    tests

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar, Button, Alert } from 'react-native';
+import { ScreenOrientation } from 'expo';
+import Canvas from 'react-native-canvas';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Party Woooooooo!</Text>
-    </View>
-  );
+
+// ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
+export default class App extends React.Component {
+
+  state = {
+    x: 10,
+    y: 10,
+    w: 10,
+    h: 10,
+    c: 'black',
+  }
+  
+  handleCanvas = (canvas) => {
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = this.state.c;
+    ctx.fillRect(this.state.x, this.state.y, this.state.w, this.state.h);
+  }
+  componentDidMount() {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <StatusBar hidden={true} />
+        <Text>Tap Accuracy Test</Text>
+        <Button
+          title="Play Now"
+          color="#f194ff"
+          onPress={this.start}
+        />
+        <Canvas  style={styles.canvas} ref={this.handleCanvas}/>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  canvas: {
+    zIndex: -1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    borderWidth: 1,
+    borderColor: 'black',
+  }
 });
