eb97156a9a71317ac73b9d01ea93da836059821d
unknown <pxgraham@gmail.com>
2020-02-05 20:42:08 -0700

    -

--- App.js
+++ App.js
@@ -1,62 +1,142 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, StatusBar, Button, Alert } from 'react-native';
-import { ScreenOrientation } from 'expo';
-import Canvas from 'react-native-canvas';
+import React from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  ImageBackground,
+  TextInput,
+} from 'react-native';
+import Constants from 'expo-constants';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
 
 
-// ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
-export default class App extends React.Component {
-
-  state = {
-    x: 10,
-    y: 10,
-    w: 10,
-    h: 10,
-    c: 'black',
-  }
-  
-  handleCanvas = (canvas) => {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = this.state.c;
-    ctx.fillRect(this.state.x, this.state.y, this.state.w, this.state.h);
-  }
-  componentDidMount() {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-  }
-  
-  render() {
-    return (
-      <View style={styles.container}>
-
-        <StatusBar hidden={true} />
-        <Text>Tap Accuracy Test</Text>
-        <Button
-          title="Play Now"
-          color="#f194ff"
-          onPress={this.start}
-        />
-        <Canvas  style={styles.canvas} ref={this.handleCanvas}/>
-
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navbar}>
+        <Text style={styles.navtext}>
+          Hidden Blue Shop
+        </Text>
       </View>
-    );
-  }
+      <View>
+        <Text style={styles.heading}>
+          Welcome!
+        </Text>
+
+        <View style={styles.inputtextcontainer}>
+          <Text style={styles.inputtext}>
+            Name
+          </Text>
+        </View>
+        <View style={styles.fixToText}>
+          <TextInput
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputtextcontainer}>
+          <Text style={styles.inputtext}>
+            Pass
+          </Text>
+        </View>
+        <View style={styles.fixToText}>
+          <TextInput
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.fixToText}>
+          <Button
+            title="                Sign In                    "
+            color="lime"
+            onPress={() => Alert.alert('Winner')}
+          />
+        </View>
+        <View style={styles.fixToText}>
+          <Button
+            title="Sign Up"
+            color="blue"
+            onPress={() => Alert.alert('Winner')}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'cyan',
-    alignItems: 'center',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 0,
+  },
+  heading: {
+    textAlign: 'center',
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 15,
+    fontSize: 50,
+    color: 'blue',
+  },
+  title: {
+    textAlign: 'center',
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 15,
+    fontSize: 35,
+    marginVertical: 0,
+    color: 'blue',
+  },
+  fixToText: {
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 15,
+    flexDirection: 'row',
     justifyContent: 'center',
   },
-  canvas: {
-    zIndex: -1,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    borderWidth: 1,
+  navbar: {
+    elevation: 10,
+    backgroundColor: 'cyan',
+    height: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  navtext: {
+    textAlign: 'center',
+    paddingTop: 2.5,
+    fontSize: 35,
+    color: 'white',
+  },
+  input: {
     borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    width: '50%',
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  inputtextcontainer: {
+    textAlign: 'left',
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  inputtext: {
+    fontSize: 20,
+    textAlign: 'left',
+    flexDirection: 'row',
+    justifyContent: 'center',
   }
 });
